ad5c8fb0f3ecc26dc094f2b2852b73cc2e82b992
Andreasgura <andre-56@hotmail.it>
2024-05-02 16:01:16 +0200

    printed table

--- index.php
+++ index.php
@@ -1,13 +1,39 @@
 <?php
 include __DIR__ . "/Models/steam.php";
+// var_dump($hotels);
 
 
-include __DIR__ ."/Views/head.php";
+include __DIR__ . "/Views/head.php";
 ?>
 
 
 
 <body>
-    
+    <main class="container">
+        <h1 class="text-center mb-5">Hotel List</h1>
+        <table class="table">
+        <thead>
+            <tr>
+                <?php
+                foreach ($hotels[0] as $key => $value) {
+                    echo "<th class='text-uppercase'>$key</th>";
+                }
+                ?>
+            </tr>
+        </thead>
+        <?php
+        foreach ($hotels as $hotel) {
+            echo "<tr>";
+            foreach ($hotel as $value) {
+                echo "<td>$value</td>";
+            }
+            echo "</tr>";
+        }
+        ;
+        ?>
+        </table>
+
+    </main>
 </body>
+
 </html>
